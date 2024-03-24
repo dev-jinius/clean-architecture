9edd8285f926b42f0cefac1ab1db1ca1dc44c51d
jinius <dev.jinius.j@gmail.com>
2024-03-24 13:26:39 +0900

    create project

---
+++ jj-clean-architecture/build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+	java
+	alias(libs.plugins.spring.boot)
+	alias(libs.plugins.spring.dependency.management)
+}
+
+group = "com.jinius.architecture"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+allprojects {
+	group = property("app.group").toString()
+}
+
+dependencyManagement {
+	imports {
+		mavenBom(libs.spring.cloud.dependencies.get().toString())
+	}
+}
+
+dependencies {
+	implementation(libs.spring.boot.starter.web)
+	implementation(libs.spring.boot.starter.data.jpa)
+	runtimeOnly(libs.h2)
+	compileOnly(libs.lombok)
+	annotationProcessor(libs.lombok)
+	annotationProcessor(libs.spring.boot.configuration.processor)
+	testImplementation(libs.spring.boot.starter.test)
+}
+
+// about source and compilation
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+// bundling tasks
+tasks.getByName("bootJar") {
+	enabled = true
+}
+tasks.getByName("jar") {
+	enabled = false
+}
+
+// test tasks
+tasks.test {
+	ignoreFailures = true
+	useJUnitPlatform()
+}
---
+++ jj-clean-architecture/src/test/java/com/jinius/architecture/clean/JjCleanArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinius.architecture.clean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjCleanArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
